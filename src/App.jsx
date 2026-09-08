5e2d942507786198035d81367d6bfa27db0eff95
samatha-reddy18 <samathareddy755@gmail.com>
2026-09-08 11:09:38 +0530

    Fix React routing and deployment

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,17 @@
 import './App.css'
-import Login from './pages/Login'
-import Home from './pages/Home'
+
 
 import { Route, Routes } from 'react-router-dom'
-import Signup from './pages/Signup'
+import Home from './pages/Home';
+import Login from './pages/Login';
+import Signup from './pages/Signup';
+
 
 function App() {
   return (
     <div>
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/" element={<Home/>} />
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
       </Routes>
