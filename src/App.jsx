12f3a43fcfb72797b5ac2832535b26e4ec68a1ed
samatha-reddy18 <samathareddy755@gmail.com>
2026-09-08 10:50:01 +0530

    Fix routing for GitHub Pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,19 @@
-
 import './App.css'
 import Login from './pages/Login'
 import Home from './pages/Home'
-import { Route, Routes } from 'react-router'
-import Signup from './pages/SignUp'
 
+import { Route, Routes } from 'react-router-dom'
+import Signup from './pages/Signup'
 
 function App() {
- // const [count, setCount] = useState(0)
-
   return (
     <div>
       <Routes>
-        <Route path="/" element={<Home/>}/>
-        <Route path="/login" element={<Login/>}/>
-        <Route path="/signup" element={<Signup/>}/>
-
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
       </Routes>
     </div>
-   
   )
 }
 
